ad034dd234e389d8ec2e188756c553a56ad5ba2e
carlosalv001 <carlosalvmtz@gmail.com>
2021-01-31 21:40:24 -0600

    Terminado
    Terminado

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './componentes/Formulario'
+import Cita from './componentes/Cita'
 
 function App() {
+
+  //Citas en localstorage 
+  let citasIniciales = JSON.parse( localStorage.getItem('citas') )
+  console.log(`Citas inciciales: ${citasIniciales.id}`)
+  if(!citasIniciales){
+    console.log("aqui")
+    citasIniciales =[]
+  }
+
+  //arreglo de citas
+  const [citas, guardarCitas] = useState(citasIniciales)
+
+  //UseEffect para ciertas operacioens cuando el state cambia 
+  useEffect(()=>{ // se ejecuta cuando el componente esta listo o cuando tiene cambios
+    let citasIniciales = JSON.parse( localStorage.getItem('citas') )
+    if(citasIniciales){
+      console.log("aqui")
+      localStorage.setItem('citas', JSON.stringify(citas))
+    }else{
+      console.log("aqui")
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+  }, [citas]) // cada vez que las citas cambies se va a ejecutar lo que esta adentro
+
+  //funcion que tome las citas actuales y agregue la nueva
+  const crearCita = (cita) =>{
+    guardarCitas([
+      ...citas,
+      cita
+    ])
+  }
+
+  //funcion que elimina una cita por su id
+  const eliminarCita=(id) => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+    guardarCitas(nuevasCitas)
+  }
+
+  //Mensaje condicional
+  const titulo = citas.length === 0 ?'No hay citas' : 'Administra tus citas'
+
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-              <Formulario/>
+              <Formulario
+                crearCita ={crearCita}
+              />
 
           </div>
           <div className="one-half column">
-            2
-            
+            <h2>{titulo}</h2>
+            {citas.map(cita =>(
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita= {eliminarCita}
+              />
+            ))}
           </div>
         </div>
       </div>
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,6 +1,8 @@
 import React, {Fragment, useState } from 'react';
+import uuid from 'uuid/dist/v4'
+import PropTypes from 'prop-types'
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 
     //Crear state de las citas
     const [cita, actualizarCita] = useState({
@@ -10,22 +12,65 @@ const Formulario = () => {
         hora:'',
         sintomas:''
     })
+
+    const [error, actualizarError] = useState(false)
+
     //Esta funcion se va a ejecutar cada vez que escriba en algun input
     const actualizarState = e =>{
-        console.log("escribiendo")
+        actualizarCita({
+            ...cita,
+            [e.target.name] : e.target.value
+        })
     }
+
+    //cuando el usuario presiona agregar cita
+    const submitCita =(e) =>{
+        e.preventDefault();
+
+        //validamos
+        if(mascota.trim() ==='' || fecha.trim() ==='' || propietario.trim() ==='' || hora.trim() ==='' ||
+         sintomas.trim() ===''){ //trim elimina los espacion en blanco
+           actualizarError(true)
+            return;
+        } 
+        //Eliminar el mensaje de que hubo un error
+        actualizarError(false)
+        
+        //asignar id
+        cita.id = uuid()
+        //Crear cita
+        crearCita(cita)
+        //reiniciar el form
+        actualizarCita({
+            mascota:'',
+            propietario:'',
+            fecha:'',
+            hora:'',
+            sintomas:''
+        })
+    }
+
+    //Extraer los valores
+    const {mascota, propietario, fecha, hora, sintomas} = cita
 
 
     return ( 
         <Fragment>
             <h2>Crear cita</h2>
-            <form>
+            {error
+            ?<p className="alerta-error">Tienes que llenar todos los campos</p>
+            :null
+            }
+            <form
+            onSubmit={submitCita}
+            >
                 <label>Nombre mascota</label>
                 <input 
                 type="text"
                 name="mascota"
                 className="u-full-width"
                 placeholder="Nombre mascota"
+                value={mascota}
                 onChange={actualizarState}/>
                 <label>Nombre del dueno</label>
                 <input 
@@ -33,12 +78,14 @@ const Formulario = () => {
                 name="propietario"
                 className="u-full-width"
                 placeholder="Nombre dueno de la mascota"
+                value={propietario}
                 onChange={actualizarState}/>
                 <label>Fecha </label>
                 <input 
                 type="date"
                 name="fecha"
                 className="u-full-width"
+                value={fecha}
                 onChange={actualizarState}
                 />
                 <label>Hora</label>
@@ -46,12 +93,14 @@ const Formulario = () => {
                 type="time"
                 name="hora"
                 className="u-full-width"
+                value={hora}
                 onChange={actualizarState}
                 />
                 <label>Sintomas</label>
                 <textarea
                 className="u-full-width"
                 name="sintomas"
+                value={sintomas}
                 onChange={actualizarState}
                 >
                 </textarea>
@@ -62,6 +111,12 @@ const Formulario = () => {
             </form>
         </Fragment>
      );
+}
+
+
+//Esto es para documentar el tipo de lo que recibe y si es obligatorio o no 
+Formulario.propTypes = {
+    crearCita: PropTypes.func.isRequired
 }
  
 export default Formulario;
